83aa46368fdede8742f52c8e0531ece7acb228c9
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-12-05 12:12:37 +0100

    not working

--- canevasTD5/shaders/basic.frag.glsl
+++ canevasTD5/shaders/basic.frag.glsl
@@ -4,10 +4,19 @@
 in vec4 color;
 
 out vec4 frag_color;
+in vec3 lightDir;
+in vec3 eyeVec;
+in vec3 out_normal;
 
 void main( void )
 {
     frag_color =  color*-1;
-   //frag_color = vec4( mod(ceil(gl_FragCoord.x/30)+ceil(gl_FragCoord.y/10), 2), 0.0, 0.0, 1.0 );
-   //frag_color = vec4( pow(cos(gl_FragCoord.x*0.02),2.0), 0.0, 0.0, 1.0 );
+    // frag_color = vec4( mod(ceil(gl_FragCoord.x/30)+ceil(gl_FragCoord.y/10), 2), 0.0, 0.0, 1.0 );
+    // frag_color = vec4( pow(cos(gl_FragCoord.x*0.02),2.0), 0.0, 0.0, 1.0 );
+
+    // vec3 L = normalize(lightDir);
+    // vec3 N = normalize(out_normal);
+
+    // float intensity = max(dot(L,N),0.0);
+    // frag_color = vec4(intensity,intensity,intensity, 1.0);
 }
--- canevasTD5/shaders/basic.vert.glsl
+++ canevasTD5/shaders/basic.vert.glsl
@@ -4,37 +4,28 @@
 // in indique que la variable est fournie en entrée pour chaque point
 // chaque point possède une position 3D
 in vec3 in_pos;
-
 in vec3 in_normal;
-out vec4 color;
 
+out vec4 color;
 out vec3 lightDir;
 out vec3 eyeVec;
 out vec3 out_normal;
 
 uniform mat4 v,m,p;
 
-out vec4 frag_color;
-
 void main(void)
 {
-  color = vec4(in_normal, 0.0);
-  // color =in_pos;
-  // calcul de la position du point une fois toutes les transformations appliquées
+    color = vec4(in_normal, 0.0);
+    // color =in_pos;
+    // calcul de la position du point une fois toutes les transformations appliquées
 
-  gl_Position = p * v * m * vec4( in_pos, 1.0 );
+    gl_Position = p*v*m * vec4(in_pos, 1.0);
 
-  vec4 vVertex = v*m * vec4(in_pos, 1.0);
-  eyeVec = -vVertex.xyz;
+    vec4 vVertex = v*m * vec4(in_pos, 1.0);
+    eyeVec = -vVertex.xyz;
 
-  vec4 LightSource_position=vec4(0.0,0.0,10.0,0.0);
-  lightDir=vec3(LightSource_position.xyz - vVertex.xyz);
+    vec4 LightSource_position=vec4(0.0,0.0,10.0,0.0);
+    lightDir=vec3(LightSource_position.xyz - vVertex.xyz);
 
-  out_normal = in_normal;
-  
-  vec3 L = normalize(lightDir);
-  vec3 N = normalize(out_normal);
-
-  float intensity = max(dot(L,N),0.0);
-  frag_color = vec4(intensity,intensity,intensity, 1.0);
+    out_normal = in_normal;
 }
