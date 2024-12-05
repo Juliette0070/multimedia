a6f02e065932a874b5c26225e223c47b3e4f8041
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-12-05 11:00:04 +0100

    Add VSCode settings, update test logs, and modify shader calculations

--- canevasTD5/shaders/basic.frag.glsl
+++ canevasTD5/shaders/basic.frag.glsl
@@ -7,7 +7,7 @@ out vec4 frag_color;
 
 void main( void )
 {
-    frag_color =  color;
+    frag_color =  color*-1;
    //frag_color = vec4( mod(ceil(gl_FragCoord.x/30)+ceil(gl_FragCoord.y/10), 2), 0.0, 0.0, 1.0 );
    //frag_color = vec4( pow(cos(gl_FragCoord.x*0.02),2.0), 0.0, 0.0, 1.0 );
 }
--- canevasTD5/shaders/basic.vert.glsl
+++ canevasTD5/shaders/basic.vert.glsl
@@ -1,10 +1,6 @@
 #version 150 core
 // version du langage GLSL utilisée, ici 4.5
 
-// mvp est la variable contenant la matrice proj*view*model
-// uniform indique que c'est la même matrice pour tous les points
-uniform mat4 mvp;
-
 // in indique que la variable est fournie en entrée pour chaque point
 // chaque point possède une position 3D
 in vec3 in_pos;
@@ -12,11 +8,33 @@ in vec3 in_pos;
 in vec3 in_normal;
 out vec4 color;
 
+out vec3 lightDir;
+out vec3 eyeVec;
+out vec3 out_normal;
+
+uniform mat4 v,m,p;
+
+out vec4 frag_color;
 
 void main(void)
-{color = vec4(in_normal, 0.0);
- //   color =in_pos;
+{
+  color = vec4(in_normal, 0.0);
+  // color =in_pos;
   // calcul de la position du point une fois toutes les transformations appliquées
+
+  gl_Position = p * v * m * vec4( in_pos, 1.0 );
+
+  vec4 vVertex = v*m * vec4(in_pos, 1.0);
+  eyeVec = -vVertex.xyz;
+
+  vec4 LightSource_position=vec4(0.0,0.0,10.0,0.0);
+  lightDir=vec3(LightSource_position.xyz - vVertex.xyz);
+
+  out_normal = in_normal;
   
-  gl_Position = mvp * vec4( in_pos, 1.0 );
+  vec3 L = normalize(lightDir);
+  vec3 N = normalize(out_normal);
+
+  float intensity = max(dot(L,N),0.0);
+  frag_color = vec4(intensity,intensity,intensity, 1.0);
 }
