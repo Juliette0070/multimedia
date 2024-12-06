06529f0e1aed424c3dfd280779d0debd9138a207
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-12-06 14:23:00 +0100

    Update .gitignore to exclude Visual Studio Code settings and remove obsolete build files

--- canevasTD5/b/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/juju/Desktop/multimedia/canevasTD5
-# Build directory: /home/juju/Desktop/multimedia/canevasTD5/b
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SimpleTest]=] "td")
-set_tests_properties([=[SimpleTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/juju/Desktop/multimedia/canevasTD5/CMakeLists.txt;71;add_test;/home/juju/Desktop/multimedia/canevasTD5/CMakeLists.txt;0;")
